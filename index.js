f0facbe2db7a2b32dc7b000b168d533e7c7259f0
Conrad Kambi <kambiconrad@gmail.com>
2023-09-23 19:25:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+// Define the initial 'cats' array
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  // Create a new array with the original cats and the new cat
+  const newCats = [...cats, name];
+  return newCats;
+}
+
+function prependCat(name) {
+  // Create a new array with the new cat and the original cats
+  const newCats = [name, ...cats];
+  return newCats;
+}
+
+function removeLastCat() {
+  // Create a new array without the last cat
+  const newCats = cats.slice(0, -1);
+  return newCats;
+}
+
+function removeFirstCat() {
+  // Create a new array without the first cat
+  const newCats = cats.slice(1);
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
+
